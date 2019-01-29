d741770966701a175a6f97103b5a0b0c83e729c4
wangpeng <379831050@qq.com>
2019-01-30 00:34:04 +0800

    生成maven插件

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -30,12 +30,12 @@ public class Car extends AbstractMojo {
       public void execute() throws MojoExecutionException, MojoFailureException {
 
           getLog().info("===== 生成代码开始 =====");
-          System.out.println( tables+ " "+ dbDriver+ " "+url+ " "+user+ " "+password+ " "+"root"+ " "+mapperJavaPath+ " "+mapperXmlPath+ " "+entityPath);
+
           if(StringUtils.isBlank(mapperJavaPath)||StringUtils.isBlank(mapperXmlPath)|| StringUtils.isBlank(entityPath)){
               getLog().error("请配置文件生成目录");
           }else{
               getLog().info("===== 生成mapperXmlPath ===== "+mapperXmlPath);
-              System.out.println( tables+ " "+ dbDriver+ " "+url+ " "+user+ " "+password+ " "+"root"+ " "+mapperJavaPath+ " "+mapperXmlPath+ " "+entityPath);
+
               JdbcTools.create(  tables,   dbDriver,   url,   user,   password,   "root",   mapperJavaPath,   mapperXmlPath,   entityPath);
           }
 
